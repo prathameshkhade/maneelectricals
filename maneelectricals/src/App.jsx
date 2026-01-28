71f4656778f331615bc8485bc5660c37f31807ac
prathameshkhade <122447678+prathameshkhade@users.noreply.github.com>
2026-01-28 19:08:12 +0530

    done: bulb component

--- maneelectricals/src/App.jsx
+++ maneelectricals/src/App.jsx
@@ -1,41 +1,33 @@
-import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
-import { useRef } from "react";
-import { SplitText } from "gsap/all";
+import Bulb from "./components/common/Bulb";
 
 export default function App() {
-  const containerRef = useRef(null);
-
-  useGSAP(() => {
-    var split = SplitText.create(containerRef.current, {type: 'chars'});
-    var tl = gsap.timeline();
-
-    tl
-      .from(split.chars, {
-        duration: 1.3,
-        ease: 'expo.out',
-        opacity: 0, 
-        y: 180,
-        autoAlpha: 0,
-        stagger: 0.07,
-      })
-      .to(split.chars, {
-        y: -180,
-        duration: 1.2,
-        ease: 'expo.in',
-        stagger: 0.07,
-        autoAlpha: 0,
-        opacity: 0
-      },
-      '-=1'
-      )
-
-    
-  }, []);
 
   return (
-    <section className="w-full h-screen bg-red-300 flex items-center p-9 justify-center overflow-hidden">
-      <h1 ref={containerRef} className="text-9xl font-bold text-white">Hello World!</h1>
-    </section>
+    <div className="w-full h-screen flex items-center justify-center">
+      {/* Magenta neon bulb */}
+      <Bulb 
+        // ref={bulbRef}
+        color="#FF10F0" 
+        width={250} 
+        height={250} 
+        rotation={15} 
+      />
+
+      {/* Yellow neon bulb */}
+      <Bulb 
+        color="#FFFF00" 
+        width={200} 
+        height={200} 
+        rotation={-20} 
+      />
+
+      {/* Cyan neon bulb */}
+      <Bulb 
+        color="#00FFFF" 
+        width={180} 
+        height={180} 
+        rotation={0} 
+      />
+    </div>
   );
 }
