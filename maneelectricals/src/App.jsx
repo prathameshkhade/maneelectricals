038f12796fbee9466486d76a4e2bf5906debd7b6
prathameshkhade <122447678+prathameshkhade@users.noreply.github.com>
2026-01-28 13:39:50 +0530

    add dependancies: gsap & tailwind

--- maneelectricals/src/App.jsx
+++ maneelectricals/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+import { SplitText } from "gsap/all";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const containerRef = useRef(null);
+
+  useGSAP(() => {
+    var split = SplitText.create(containerRef.current, {type: 'chars'});
+    var tl = gsap.timeline();
+
+    tl
+      .from(split.chars, {
+        duration: 1.3,
+        ease: 'expo.out',
+        opacity: 0, 
+        y: 180,
+        autoAlpha: 0,
+        stagger: 0.07,
+      })
+      .to(split.chars, {
+        y: -180,
+        duration: 1.2,
+        ease: 'expo.in',
+        stagger: 0.07,
+        autoAlpha: 0,
+        opacity: 0
+      },
+      '-=1'
+      )
+
+    
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <section className="w-full h-screen bg-red-300 flex items-center p-9 justify-center overflow-hidden">
+      <h1 ref={containerRef} className="text-9xl font-bold text-white">Hello World!</h1>
+    </section>
+  );
 }
-
-export default App
