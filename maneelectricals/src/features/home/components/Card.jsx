868fc757a8712e62c8d19160b07d4671824f62c0
prathameshkhade <122447678+prathameshkhade@users.noreply.github.com>
2026-02-01 11:38:08 +0530

    fix: card height was low

--- maneelectricals/src/features/home/components/Card.jsx
+++ maneelectricals/src/features/home/components/Card.jsx
@@ -1,9 +1,10 @@
 export default function Card({ feature, index }) {
     return (
         <div 
+            id = 'card'
             key={index}
             style={{ outlineColor: feature.color }} 
-            className="bg-white/15  w-[30vw] rounded-[3vw] py-10 px-8 flex flex-col gap-6 outline-2"
+            className="bg-white/15 w-[30vw] rounded-[3vw] py-10 px-8 flex flex-col gap-6 outline-2"
         >
             <div className="w-16 h-16 bg-white/50 flex items-center p-3 rounded-[2vw]">
                 <feature.icon width={60} height={60} />
