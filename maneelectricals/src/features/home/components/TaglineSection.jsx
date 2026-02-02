3f13744646660901fca9412534df16c888a27688
prathameshkhade <122447678+prathameshkhade@users.noreply.github.com>
2026-02-02 22:19:11 +0530

    add: tagline animation

--- maneelectricals/src/features/home/components/TaglineSection.jsx
+++ maneelectricals/src/features/home/components/TaglineSection.jsx
@@ -3,30 +3,35 @@ import Colors from '../../../config/theme/colors/colors';
 
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
+import { Flip } from 'gsap/all';
+
+gsap.registerPlugin(Flip)
 
 const TaglineSection = () => {
     const taglinesRef = useRef(null);
 
     useGSAP(() => {
+        // Configure the timeline with ScrollTrigger
         const timeline = gsap.timeline({
             scrollTrigger: {
                 trigger: taglinesRef.current,
                 start: 'center center',
                 end: 'bottom bottom',
-                pin: true,
-                markers: true,
-                toggleActions: 'play none none reverse'
+                pin: true,s
             }}
         );
 
-        timeline.set('#hero', { yPercent: 100 })
-        gsap.utils.toArray('#hero h2').forEach((tagline, idx) => {
+        // Collect all the h2 of #hero
+        var heroTexts = gsap.utils.toArray('#hero h2');
+
+        timeline.set('#hero', { yPercent: 100 })  // Move #hero to -100% on y axis
+        heroTexts.forEach((tagline) => {
             timeline.set(tagline, { opacity:1 })
             
             timeline.from(tagline, {
                 opacity: 0,
-                duration: 0.7,
-                yPercent: 120,
+                duration: 0.9,
+                yPercent: 125,
                 scale: 0.6,
                 ease: 'power4.out',
             })
@@ -35,13 +40,24 @@ const TaglineSection = () => {
                 yPercent: '-=25',
                 duration: 0.7,
                 ease: 'power1.out',
-            }, '-=0.5')
+            }, '<')
+        });
+
+        timeline.call(() => {
+            const state = Flip.getState('#hero h2');
+            const hero = document.querySelector('#hero');
+            hero.classList.replace('items-center', 'items-start');
+        
+            Flip.from(state, {
+                duration: 0.8,
+                ease: 'power1.out',
+            });
         });
     });
     
     return (
-        <section ref={taglinesRef} className="py-[10vh] relative w-full flex flex-col items-center justify-center text-[7vw] font-extrabold">
-            <div id='hero' className='w-full flex flex-col items-center'>
+        <section ref={taglinesRef} className="py-[10vh] relative w-full flex flex-col justify-center text-[7vw] font-extrabold">
+            <div id='hero' className='w-full flex flex-col items-center will-change-transform'>
                 <h2>
                     <span>POWERING YOUR </span>
                     <span style={{color : Colors.yellow}}>DREAMS</span>
